c7f6b192e359f4f5dbe9b56716ac13ea57b78d50
brahanvasquez <115602117+brahanvasquez@users.noreply.github.com>
2022-11-17 22:13:51 -0600

    Otros 30 minutos
    commit

--- AplicacionWeb/Modelos/Usuario.cs
+++ AplicacionWeb/Modelos/Usuario.cs
@@ -8,6 +8,11 @@ namespace Modelos
 {
     public class Usuario
     {
-
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Clave { get; set; }
+        public string? Correo { get; set; }
+        public string Rol { get; set; }
+        public bool EstaActivo { get; set; }
     }
 }
