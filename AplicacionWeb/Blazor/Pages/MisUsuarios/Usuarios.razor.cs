80109ea065e4bd550fddfe3f6b0c14a78ec99b97
brahanvasquez <115602117+brahanvasquez@users.noreply.github.com>
2022-11-21 17:45:53 -0600

    commit usuarios
    commit de mostrar usuarios

--- AplicacionWeb/Blazor/Pages/MisUsuarios/Usuarios.razor.cs
+++ AplicacionWeb/Blazor/Pages/MisUsuarios/Usuarios.razor.cs
@@ -10,5 +10,9 @@ namespace Blazor.Pages.MisUsuarios
 
         private IEnumerable<Usuario> lista { get; set; }
 
+        protected override async Task OnInitializedAsync()
+        {
+            lista = await usuarioServicio.GetLista();
+        }
     }
 }
